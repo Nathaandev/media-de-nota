e1183efea7e97f3281473e5aab7c715cad2e8495
Nathan da Silva <111094286+Nathaandev@users.noreply.github.com>
2023-02-26 22:06:16 -0300

    Add files via upload

---
+++ média de nota.py
@@ -0,0 +1,7 @@
+n1=float(input('Coloque a nota da primeira prova do aluno: '))
+
+n2=float(input('Coloque a nota da segunda prova do aluno: '))
+
+soma=n1+n2
+
+print('A nota da primeira da prova do aluno é {} e a da segunda prova é {}, a média do aluno é {}' .format(n1, n2, soma/2))
